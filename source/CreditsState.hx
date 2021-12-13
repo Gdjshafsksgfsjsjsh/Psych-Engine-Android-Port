85eef8a5aff3663e767138b74ae88fb25b6ebf38
Gdjshafsksgfsjsjsh <95997326+Gdjshafsksgfsjsjsh@users.noreply.github.com>
2021-12-13 13:34:24 +0700

    Add files via upload

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -46,10 +46,10 @@ class CreditsState extends MusicBeatState
 		add(grpOptions);
 
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
-			['Android Port'],
-			['M.A. Jigsaw',		    'majigsaw',		    'Main Coder of The Port',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
-			['Psych Engine Team'],
-			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'0xFFFFDD33'],
+			['CODE'],
+			['TOPPY CHANNEL',		    'toppy channel',		    'Main Coder of The Port',	 'https://youtube.com/channel/UCBR0h75XPErVq40d0Fhqs9Q',	'0xFFC30085'],
+			['more'],
+			['Myzen99',		'myzen99',		'Main Programmer of Psych Engine',					'https://youtube.com/channel/UCA26PLhawaW6Dlzw26wevgQ',	'0xFFFFDD33'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',				'https://twitter.com/river_oaken',		'0xFFC30085'],
 			[''],
 			['Engine Contributors'],
